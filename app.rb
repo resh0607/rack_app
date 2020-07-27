fad2da22b55bceea52577ac441bff7509278c764
Ivan Reshetnyak <resh0607@gmail.com>
2020-07-27 22:54:17 +0300

    update rack_time app

--- app.rb
+++ app.rb
@@ -5,23 +5,47 @@ class App
 
   def call(env)
     request = Rack::Request.new(env)
-  
-    if request.get? && !request.params.empty?
-      operate_request(request)
-    else
-      Rack::Response.new('Can`t serve your request')
-    end
+
+    request_valid?(request) ? operate_request(request) : response_bad_request
   end
+
+  private
 
   def operate_request(request)
     params_string = request.params['format']
-    formatted_time = TimeFormatter.new(params_string)
+    @time_formatter = TimeFormatter.new(params_string)
 
-    if formatted_time.valid?
-      Rack::Response.new("#{formatted_time.converted_formats}")
-    else
-      Rack::Response.new("Unknown formats #{formatted_time.rejected_params}")
-    end
+    request_query_string_valid? ? response_success : response_unknown_formats
+  end
+
+  def request_query_string_valid?
+    @time_formatter.rejected_params.empty?
+  end
+
+  def request_valid?(request)
+    request.get? &&
+    request.path == '/time' &&
+    !request.params.empty? &&
+    !request.params['format'].nil?
+  end
+
+  def response(options)
+    [
+      options[:status],
+      {'Content-Type' => "text/html"},
+      [options[:body]]
+    ]
+  end
+
+  def response_bad_request
+    response({status: 404, body: 'Check your request'})
+  end
+
+  def response_success
+    response({status: 200, body: @time_formatter.converted_formats})
+  end
+
+  def response_unknown_formats
+    response({status: 400, body: "Unknown formats #{@time_formatter.rejected_params}"})
   end
 end
-
--- lib/time_formatter.rb
+++ lib/time_formatter.rb
@@ -7,27 +7,25 @@ class TimeFormatter
               month:  '%m',
               day:    '%d',
               hour:   '%H',
-              minute: '%m',
-              second: 'S'
-            }
+              minute: '%M',
+              second: '%S'
+  }.freeze
 
   def initialize(params_string)
     @params_string = params_string
     check_formats
   end
 
-  def check_formats
-      params = @params_string.split(',')
-      @accepted_params = params.select { |p| FORMATS.key?(p.to_sym) }
-      @rejected_params = params - @accepted_params
-  end
-
-  def valid?
-    @rejected_params.empty?
-  end
-
   def converted_formats
     time_format = @accepted_params.map { |p| FORMATS[p.to_sym] }
     Time.now.strftime(time_format.join('-'))
+  end
+
+  private
+
+  def check_formats
+    params = @params_string.split(',')
+    @accepted_params = params.select { |p| FORMATS.key?(p.to_sym) }
+    @rejected_params = params - @accepted_params
   end
 end
